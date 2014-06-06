95c1c0fed87559f28939d970fae68ec9c23b423b
Euziane <euziane@gmal.com>
2014-06-06 10:42:16 -0300

    Quebrando um test para falhar no Jenkins

--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -22,7 +22,7 @@ public class EspetaculoTest {
 		ivete.getSessoes().add(sessaoComIngressosSobrando(3));
 		ivete.getSessoes().add(sessaoComIngressosSobrando(2));
 
-		assertTrue(ivete.Vagas(5));
+		assertTrue(ivete.Vaga(5));
 	}
 
 	@Test
